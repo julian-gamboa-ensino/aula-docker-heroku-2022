8ad8173840e2bc0955dde7ff4e9e7e21eb6949e9
julian-gamboa-ensino <78002411+julian-gamboa-ensino@users.noreply.github.com>
2022-03-19 23:03:24 -0300

    Delete etiquetas directory

--- etiquetas/amanha/amanha/Function.js
+++
@@ -1,9 +0,0 @@
-
-
-module.exports = function (firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.fullName = function () { 
-        return this.firstName + ' ' + this.lastName;
-    }
-}
